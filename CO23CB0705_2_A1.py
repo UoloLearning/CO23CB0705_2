f265e8a38ccd0eef2acf97db2155c1ba6f3d6d00
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-22 18:07:26 +0530

    Update CO23CB0705_2_A1.py

--- CO23CB0705_2_A1.py
+++ CO23CB0705_2_A1.py
@@ -1,1 +1,10 @@
+fruit_rates = {
+    "apple": 80,       # per kilogram
+    "banana": 40,      # per dozen
+}
+apple_rate = fruit_rates["apple"]
+print("The rate of apples is:", apple_rate, "INR per kilogram")
+banana_rate = fruit_rates["banana"]
+print("The rate of bananas is:", banana_rate, "INR per dozen")
 
+f
