30e277573c2dee2328bf7cc8ef87a7c64c53c86c
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-23 11:54:36 +0530

    Update and rename CO23CB0705_2_AA1py to CO23CB0705_2_AA1.py

---
+++ CO23CB0705_2_AA1.py
@@ -0,0 +1,13 @@
+# Create a dictionary called names
+names = {
+    'My Name': 'Conji',
+    'My Robot Friend': 'Mel',
+    'My Wizard Friend': 'Eva',
+    'My Nemesis': 'Lord Ero'
+}
+
+# Remove a name from the dictionary
+del names['My Name']
+
+# Print the updated dictionary
+print(names)
