902198aa303c67d485f0a7f6babf68a2d0c64c7a
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-23 12:13:45 +0530

    Update CO23CB0705_2_AA2.py

--- CO23CB0705_2_AA2.py
+++ CO23CB0705_2_AA2.py
@@ -1,1 +1,16 @@
+# Create a dictionary to store student details
+student = {
+    'Name': 'Conji',
+    'Grade': 7,
+    'Address': '123, Main Street, Avora',
+    'PhoneNumber': '123-456-7890',
+    'FavouriteSubject': 'Potions'
+}
 
+# Display student details
+print("Student Details:")
+print("Name:", student['Name'])
+print("Grade:", student['Grade'])
+print("Address:", student['Address'])
+print("Phone Number:", student['PhoneNumber'])
+print("Favorite Subject:", student['FavouriteSubject'])
